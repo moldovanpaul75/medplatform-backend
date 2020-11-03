350968eb4dfbfaad11efe626d4c4609ab0f158e5
moldovan.paul75 <moldovan.paul75@gmail.com>
2020-11-03 16:45:24 +0200

    Add JwtUtils to validate/generate jwt tokens, Add JwtFilter doFilterInternal

---
+++ medplatform_backend/src/main/java/ro/tuc/ds2020/security/jwt/JwtUtils.java
@@ -0,0 +1,66 @@
+package ro.tuc.ds2020.security.jwt;
+
+import io.jsonwebtoken.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.security.core.Authentication;
+import ro.tuc.ds2020.dtos.UserAuthenticationDTO;
+
+import java.util.Date;
+
+
+@Component
+public class JwtUtils {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(JwtUtils.class);
+
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+    @Value("${jwt.expiration}")
+    private int jwtExpirationMs;
+
+    public String generateJwtToken(Authentication authentication){
+
+        UserAuthenticationDTO userAuth = (UserAuthenticationDTO) authentication.getPrincipal();
+
+        return Jwts.builder()
+                .setSubject((userAuth.getUsername()))
+                .setIssuedAt(new Date())
+                .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .compact();
+    }
+
+    public String getUsernameFromJwtToken(String authToken){
+        return Jwts.parser()
+                .setSigningKey(jwtSecret)
+                .parseClaimsJws(authToken)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean validateJwtToken(String authToken){
+
+        try {
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(authToken);
+            return true;
+        } catch (SignatureException e) {
+            LOGGER.error("Invalid JWT signature: {}", e.getMessage());
+        } catch (MalformedJwtException e) {
+            LOGGER.error("Invalid JWT token: {}", e.getMessage());
+        } catch (ExpiredJwtException e) {
+            LOGGER.error("JWT token is expired: {}", e.getMessage());
+        } catch (UnsupportedJwtException e) {
+            LOGGER.error("JWT token is unsupported: {}", e.getMessage());
+        } catch (IllegalArgumentException e) {
+            LOGGER.error("JWT claims string is empty: {}", e.getMessage());
+        }
+
+        return false;
+    }
+}
+
+
